080864ed15e49a8d175c1a89e20ae897ed43bd37
jignesh sharma <jigneshsharma9868@gmail.com>
2023-11-09 20:57:23 +0530

    just learned hwo to configure the driver.js to create the website tour.

---
+++ js/tour.js
@@ -0,0 +1,44 @@
+const driver = window.driver.js.driver;
+
+const driverObj = driver({
+  showProgress: true,
+  steps: [
+    {
+        element: '#nav',
+        popover: {
+          title: 'Navigation Section',
+          description: 'This is the navigation section that allows you to quickly move to the respective sections.'
+        }
+    },
+    {
+      element: '#about',
+      popover: {
+        title: 'Welcome to About Section',
+        description: 'This is the About section of my portfolio where you can learn more about me.'
+      }
+    },
+    {
+      element: '#skills',
+      popover: {
+        title: 'Explore My Skills',
+        description: 'In this section, you can find information about my technical skills and expertise.'
+      }
+    },
+    {
+      element: '#contact',
+      popover: {
+        title: 'Get in Touch',
+        description: 'The Contact section provides my contact information. Feel free to reach out!'
+      }
+    },
+    {
+      element: '#projects',
+      popover: {
+        title: 'Discover My Projects',
+        description: 'Here you can check out the projects I have worked on. Click "Next" to explore!'
+      }
+    }
+  ]
+});
+
+driverObj.drive();
